8828c73b6ea31a4cc1e599eb05e7821490f93e55
Noblette <noblette.tsimihanta@gmail.com>
2026-05-02 17:15:20 +0300

    feat: update login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,30 +1,49 @@
-import React from "react";
-import "../index.css";
+import React, { useState } from "react";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    const response = await fetch("http://127.0.0.1:5000/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    });
+
+    const data = await response.json();
+    alert(data.message);
+  };
+
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-green-100 via-green-50 to-white">
-      <div className="bg-white shadow-lg rounded-2xl p-8 w-full max-w-md border border-green-100">
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-indigo-100 via-purple-50 to-white">
+      <div className="bg-white shadow-lg rounded-2xl p-8 w-full max-w-md border border-purple-100">
         <h2 className="text-2xl font-bold text-center text-green-700 mb-6">
           Login
         </h2>
 
-        <form className="space-y-4">
+        <form onSubmit={handleLogin} className="space-y-4">
           <input
             type="email"
             placeholder="Email"
-            className="w-full p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-green-400"
+            onChange={(e) => setEmail(e.target.value)}
+            className="w-full p-3 border rounded-lg"
           />
 
           <input
             type="password"
             placeholder="Password"
-            className="w-full p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-green-400"
+            onChange={(e) => setPassword(e.target.value)}
+            className="w-full p-3 border rounded-lg"
           />
 
           <button
             type="submit"
-            className="w-full bg-green-600 text-white py-3 rounded-lg hover:bg-green-700 transition"
+            className="w-full bg-green-600 text-white py-3 rounded-lg"
           >
             Login
           </button>
